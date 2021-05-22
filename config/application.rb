1c9a9028aab68fdef82b3cfa7a5b1874b50b34a9
nkanemitsu <nariaki.kanemitsu@gmail.com>
2021-05-22 14:50:33 +0900

    Create Show action

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module AlphaBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # rails routes 覚えのないroutingが表示される対策
+    # ここから
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+    app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+    app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+    }
+    # ここまで追加
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
